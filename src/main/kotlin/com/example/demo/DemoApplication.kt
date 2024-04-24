f3ada01a1c0e737f8b324757c873cc5b905a3127
Miguel Fernández <mikeloguay@gmail.com>
2024-04-24 10:07:52 +0200

    feat: JPA

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,11 +2,13 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.*
 import java.util.*
-import org.springframework.jdbc.core.query
+
 
 @SpringBootApplication
 class DemoApplication
@@ -15,7 +17,8 @@ fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
 
-data class Message(val id: String?, val text: String)
+@Table("MESSAGES")
+data class Message(@Id var id: String?, val text: String)
 
 @RestController
 class MessageController(val service: MessageService) {
@@ -32,20 +35,17 @@ class MessageController(val service: MessageService) {
 }
 
 @Service
-class MessageService(val db: JdbcTemplate) {
-	fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
-		Message(response.getString("id"), response.getString("text"))
-	}
+class MessageService(val db: MessageRepository) {
+	fun findMessages(): List<Message> = db.findAll().toList()
+
+	fun findMessageById(id: String): List<Message> = db.findById(id).toList()
 
 	fun save(message: Message) {
-		val id = message.id ?: UUID.randomUUID().toString()
-		db.update(
-			"insert into messages values ( ?, ? )",
-			id, message.text
-		)
+		db.save(message)
 	}
 
-	fun findMessageById(id: String): List<Message> = db.query("select * from messages where id = ?", id) { response, _ ->
-		Message(response.getString("id"), response.getString("text"))
-	}
+	fun <T : Any> Optional<out T>.toList(): List<T> =
+		if (isPresent) listOf(get()) else emptyList()
 }
+
+interface MessageRepository : CrudRepository<Message, String>
